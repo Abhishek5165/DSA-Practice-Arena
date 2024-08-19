0a4020101b64bc3f265a1936e994e61cf73d9c93
abhishekch33 <abhishek.2101134ec@iiitbh.ac.in>
2024-08-19 23:21:58 +0530

    modified

---
+++ Arrays/Find_Duplicate_Integer.cpp
@@ -0,0 +1,25 @@
+Time complexity : O(n);
+Space complexity : O(1);
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+    int slow = 0;
+    int fast = 0;
+    while(true){
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+    if(slow == fast){
+        break;
+    }
+    }
+    int slow2 = 0;
+    while(true){
+    slow = nums[slow];
+    slow2 = nums[slow2];
+    if(slow == slow2){
+    return slow;
+    }
+    }
+    }
+};
